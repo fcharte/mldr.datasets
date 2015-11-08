e6610f13781d53e5d547a3e185095d23149a7091
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-08 20:21:19 +0100

    Adds birds dataset

--- R/20ng.R
+++ R/20ng.R
@@ -1,4 +1,4 @@
-#' Newsgroups messages classification dataset
+#' Dataset with news messages and the news groups they belong to
 #'
 #' @description Multilabel dataset from the text domain. The original name of the dataset is \code{20ng}
 #' @format An mldr object with 19300 instances, 1006 attributes and 20 labels
