abc7f162ca4bb913054ecfaf2c23d6ff1d56f466
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 13:29:47 +0100

    Adds stackex_coffee dataset

---
+++ R/stackex_coffee.R
@@ -0,0 +1,9 @@
+#'  Dataset from the Stack Exchange's coffee forum
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 225 instances, 1763 attributes and 123 labels
+#' @source Charte, Francisco and Rivera, Antonio J. and del Jesus, Maria J. and Herrera, Francisco, "QUINTA: A question tagging assistant to improve the answering ratio in electronic forums", in EUROCON 2015 - International Conference on Computer as a Tool (EUROCON), IEEE, pp. 1-6, 2015
+#' @examples
+#' toBibtex(stackex_coffee)
+#' stackex_coffee$measures
+"stackex_coffee"
