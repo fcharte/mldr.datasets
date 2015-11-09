71c15d3ae848ba407316f0eff556f8ad1728ebc2
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 19:45:27 +0100

    Adds the eurlexsm_tra dataset

---
+++ R/eurlexsm_tra.R
@@ -0,0 +1,10 @@
+#'  List  with 10 folds of the train data from the EUR-Lex subject matters dataset
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 17413 instances, 5000 attributes and 201 labels
+#' @source Mencia, E. L. and Furnkranz, J., "Efficient pairwise multilabel classification for large-scale problems in the legal domain", Machine Learning and Knowledge Discovery in Databases, pp. 50--65, 2008
+#' @examples
+#' toBibtex(eurlexsm_tra[[1]])
+#' eurlexsm_tra[[1]]$measures
+#' @export
+eurlexsm_tra <- function() check_n_load.mldr('eurlexsm_tra')
