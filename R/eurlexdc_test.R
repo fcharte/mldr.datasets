456fa1d37e8c4b030d57af7ea0531d50255e4b6f
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 18:44:11 +0100

    Adds the eurlexdc_test dataset

---
+++ R/eurlexdc_test.R
@@ -0,0 +1,10 @@
+#'  List  with 10 folds of the test data from the EUR-Lex directory codes dataset
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 1935 instances, 5000 attributes and 412 labels
+#' @source Mencia, E. L. and Furnkranz, J., "Efficient pairwise multilabel classification for large-scale problems in the legal domain", Machine Learning and Knowledge Discovery in Databases, pp. 50--65, 2008
+#' @examples
+#' toBibtex(eurlexdc-test)
+#' eurlexdc-test$measures
+#' @export
+eurlexdc_test <- function() check_n_load.mldr('eurlexdc_test')
