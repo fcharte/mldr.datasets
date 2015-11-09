1a921c6fe71855f3ca45d929f7ae62cb1def7ef3
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 19:00:00 +0100

    Fixes examples in eurlexdc_tra and eurlexdc_test

--- R/eurlexdc_tra.R
+++ R/eurlexdc_tra.R
@@ -4,7 +4,7 @@
 #' @format An mldr object with 17413 instances, 5000 attributes and 412 labels
 #' @source Mencia, E. L. and Furnkranz, J., "Efficient pairwise multilabel classification for large-scale problems in the legal domain", Machine Learning and Knowledge Discovery in Databases, pp. 50--65, 2008
 #' @examples
-#' toBibtex(eurlexdc_test)
-#' eurlexdc_test$measures
+#' toBibtex(eurlexdc_test[[1]])
+#' eurlexdc_test[[1]]$measures
 #' @export
 eurlexdc_tra <- function() check_n_load.mldr('eurlexdc_tra')
