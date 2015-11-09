7757962a439bba3a0780cb194129278214bb3e73
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 17:09:16 +0100

    Adds reutersk500 dataset

---
+++ R/reutersk500.R
@@ -0,0 +1,10 @@
+#' Dataset from the Reuters Corpus with the 500 most relevant features selected
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 6000 instances, 500 attributes and 103 labels
+#' @source Read, Jesse, "Scalable multi-label classification", University of Waikato, 2010
+#' @examples
+#' toBibtex(reutersk500)
+#' reutersk500$measures
+#' @export
+reutersk500 <- function() check_n_load.mldr('reutersk500')
