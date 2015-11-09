bfd8fd9b273b6004b518d2d49bf97e0b9746dfbf
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 16:43:39 +0100

    Adds langlog dataset

---
+++ R/langlog.R
@@ -0,0 +1,9 @@
+#'  Dataset with data from the Language forum discussion
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 1460 instances, 1004 attributes and 75 labels
+#' @source Read, Jesse, "Scalable multi-label classification", University of Waikato, 2010
+#' @examples
+#' toBibtex(langlog)
+#' langlog$measures
+"langlog"
