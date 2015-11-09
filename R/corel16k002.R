6199f65d261ea668e9686f8217b8551bcbb92054
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-09 17:56:37 +0100

    Adds corel16k002 dataset

---
+++ R/corel16k002.R
@@ -0,0 +1,10 @@
+#'  Datasets with data from the Corel image collection. There are 10 subsets in corel16k
+#'
+#' @description Multilabel dataset from the image domain.
+#' @format An mldr object with 13761 instances, 500 attributes and 164 labels
+#' @source Barnard, K. and Duygulu, P. and Forsyth, D. and de Freitas, N. and Blei, D. M. and Jordan, M. I., "Matching words and pictures", Journal of Machine Learning Research, Vol. 3, pp. 1107--1135, 2003
+#' @examples
+#' toBibtex(corel16k001)
+#' corel16k001$measures
+#' @export
+corel16k002 <- function() check_n_load.mldr('corel16k002')
