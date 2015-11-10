852555352ca4f770592b7051696809874d0f8896
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-10 16:56:53 +0100

    Adds yahoo_computers dataset

---
+++ R/yahoo_computers.R
@@ -0,0 +1,10 @@
+#' Dataset generated from the Yahoo! web site index (computers category)
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 12444 instances, 34096 attributes and 33 labels
+#' @source Ueda, N. and Saito, K., "Parametric mixture models for multi-labeled text", Advances in neural information processing systems, pp. 721--728, 2002
+#' @examples
+#' toBibtex(yahoo_computers)
+#' yahoo_computers$measures
+#' @export
+yahoo_computers <- function() check_n_load.mldr('yahoo_computers')
