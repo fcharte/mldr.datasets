c33f4d45ab4f3017c623eb9b71df66f04ba0c8cd
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-10 16:59:42 +0100

    Adds the yahoo_education dataset

---
+++ R/yahoo_education.R
@@ -0,0 +1,10 @@
+#' Dataset generated from the Yahoo! web site index (arts education)
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 12030 instances, 27534 attributes and 33 labels
+#' @source Ueda, N. and Saito, K., "Parametric mixture models for multi-labeled text", Advances in neural information processing systems, pp. 721--728, 2002
+#' @examples
+#' toBibtex(yahoo_education)
+#' yahoo_education$measures
+#' @export
+yahoo_education <- function() check_n_load.mldr('yahoo_education')
