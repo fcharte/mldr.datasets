46388b893fb7505958e03ac6976ae550ca705831
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-10 13:55:18 +0100

    Adds the yahoo_arts dataset

---
+++ R/yahoo_arts.R
@@ -0,0 +1,10 @@
+#' Dataset generated from the Yahoo! web site index (arts category)
+#'
+#' @description Multilabel dataset from the text domain.
+#' @format An mldr object with 7484 instances, 23146 attributes and 26 labels
+#' @source Ueda, N. and Saito, K., "Parametric mixture models for multi-labeled text", Advances in neural information processing systems, pp. 721--728, 2002
+#' @examples
+#' toBibtex(yahoo_arts)
+#' yahoo_arts$measures
+#' @export
+yahoo_arts <- function() check_n_load.mldr('yahoo_arts')
