11154f7f8e7ae5851c88dc4905dc9b476422352e
David Charte <fdavidcl@outlook.com>
2015-11-16 01:18:08 +0100

    Progress in exporting functions
    A lot of code taken from mldr's own write_arff function.
    
    TODO: export sparse data, improve CSV output (?)

--- R/export.R
+++ R/export.R
@@ -4,54 +4,149 @@
 # write.mulan, write.meka, write.keel, write.csv
 
 # @relation and @data are common to MULAN, MEKA and KEEL, only the header changes and, in the KEEL case, it is necessary to
-# establish which attributes are @inputs and @outputs. Divide this tasks in separate functions
+# establish which attributes are @inputs and @outputs. Divide these tasks in separate functions
 
 # To write a CSV an indicator will be needed, setting if a header has to be included or not. The function will generate two
 # files, one with the data (i.e. mld.data.csv) and another one with the labels (i.e. mld.labels.csv). The latter will provide
 # the names and indexes of the labels
 
 
-# Generic function that will open the file, check parameters, etc., then calling the proper write.FORMAT function
-write.mldr <- function(mld, format = c("MULAN", "MEKA", "KEEL", "CSV"), sparse = FALSE, ...) {
-  # if(format == "MULAN") write.mulan(mld, connection ...)
+# 'write' method for mldr that will open the file, check parameters, etc., then call the proper write.FORMAT function
+#' @export
+write.mldr <- function(mld, format = c("MULAN", "MEKA", "KEEL", "CSV"), sparse = FALSE, basename = ifelse(!is.null(mld$name) && nchar(mld$name) > 0,
+                                                                                                          mld$name,
+                                                                                                          "unnamed_mldr"), ...) {
+  format <- toupper(format)
+  available.formats <- c("MULAN", "MEKA", "KEEL", "CSV")
+
+  # Parameter checks
+  if (!all(format %in% available.formats)) {
+    stop("Invalid format found. Allowed formats: ", do.call(paste, as.list(available.formats)))
+  }
+  if (!"mldr" %in% class(mld)) {
+    stop("Object must be of class mldr")
+  }
+
+  if ("MULAN" %in% format) {
+    # Open and write ARFF file
+    if (!"MEKA" %in% format) {
+      arffConnection <- file(paste0(basename, ".arff"))
+      writeChar(export.mulan(mld, sparse), arffConnection)
+    }
+
+    # Open and write XML file
+    xmlConnection <- file(paste0(basename, ".xml"))
+    writeChar(export.xml(mld), xmlConnection)
+  }
+
+  if ("MEKA" %in% format) {
+    # Open and write ARFF file
+    arffConnection <- file(paste0(basename, ".arff"))
+    writeChar(export.meka(mld, sparse), arffConnection)
+  }
+
+  if ("KEEL" %in% format) {
+    # Open and write DAT file
+    datConnection <- file(paste0(basename, ".dat"))
+    writeChar(export.keel(mld, sparse), datConnection)
+  }
+
+  if ("CSV" %in% format) {
+    # Open and write CSV file
+    csvConnection <- file(paste0(basename, ".csv"))
+    writeChar(export.csv(mld, sparse), csvConnection)
+  }
 }
 
-write.mulan <- function(mld, connection, sparse) {
-  write.mulan.header(mld)
-  write.arff.body(mld, sparse)
+export.mulan <- function(mld, sparse) {
+  paste(
+    export.mulan.header(mld),
+    export.arff.attributes(mld),
+    export.arff.data(mld, sparse),
+    sep = "\n"
+  )
 }
 
-write.meka <- function(mld) {
-
+export.meka <- function(mld, sparse) {
+  paste(
+    export.meka.header(mld),
+    export.arff.attributes(mld),
+    export.arff.data(mld, sparse),
+    sep = "\n"
+  )
 }
 
-write.keel <- function(mld) {
-
+export.keel <- function(mld) {
+  paste(
+    export.keel.header(mld),
+    export.arff.attributes(mld),
+    export.arff.inputs(mld),
+    export.arff.outputs(mld),
+    export.arff.data(mld, sparse),
+    sep = "\n"
+  )
 }
 
-write.mulan.header <- function(mld) {
-
+export.mulan.header <- function(mld) {
+  paste0("@relation ", ifelse(!is.null(mld$name) && nchar(mld$name) > 0,
+                             mld$name,
+                             "unnamed_mldr"))
 }
 
-
-write.meka.header <- function(mld) {
-
+export.meka.header <- function(mld) {
+  paste0("@relation '", ifelse(!is.null(mld$name) && nchar(mld$name) > 0,
+                             mld$name,
+                             "unnamed_mldr"),
+        ": -C ", mld$measures$num.labels, "'")
 }
 
+export.keel.header <- export.mulan.header
 
-write.keel.header <- function(mld) {
-
+export.arff.attributes <- function(mld) {
+  paste0("@attribute ", names(mld$attributes), " ", mld$attributes)
 }
 
-write.arff.body <- function(mld, sparse) {
-
-
+export.arff.inputs <- function(mld) {
+  paste0(
+    "@inputs ", do.call(
+      paste,
+      c(
+        as.list(names(mld$attributes[mld$attributesIndexes])),
+        sep = ", "
+      )
+    )
+  )
 }
 
-write.csv.mldr <- function(mld) {
-
+export.arff.outputs <- function(mld) {
+  paste0(
+    "@outputs ", do.call(
+      paste,
+      c(
+        as.list(rownames(mld$labels)),
+        sep = ", "
+      )
+    )
+  )
 }
 
-write.xml <- function(mld) {
+export.arff.data <- function(mld, sparse) {
+  if (sparse) {
 
+  } else {
+    data <- obj$dataset[, 1:obj$measures$num.attributes]
+    data[is.na(data)] <- '0' # NAs aren't missing values ('?') but 0
+    data <- apply(data, 1, function(c) paste(c, collapse = ','))
+  }
+}
+
+export.csv <- export.arff.data
+
+export.xml <- function(mld) {
+  xmlheader <- '<?xml version="1.0" encoding="utf-8"?>'
+  labelstag <- '<labels xmlns="http://mulan.sourceforge.net/labels">'
+  labeltags <- paste(c('<label name="'), rownames(obj$labels), c('"></label>'), sep = "\n")
+  labelsend <- '</labels>'
+
+  paste(xmlheader, labelstag, labeltags, labelsend, sep = "\n")
 }
