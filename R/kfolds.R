4ac59c2f2e15e04bbcd0f7b68c0ba2222c16b584
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-20 11:26:25 +0100

    Implements stratified sampling of mldr objects

--- R/kfolds.R
+++ R/kfolds.R
@@ -40,6 +40,7 @@ stratified.kfolds <- function(mld, k = 5, seed = 10) {
   internal.kfolds(mld, k, seed, "stratified")
 }
 
+# Partitions an mldr object using random or stratified strategy
 internal.kfolds <- function(mld, k, seed, type = "random") {
   if (class(mld) != 'mldr')
     stop(paste(substitute(mld), "isn't an mldr object"))
@@ -54,15 +55,27 @@ internal.kfolds <- function(mld, k, seed, type = "random") {
 
   if(type == "random") {
     dataset <- mld$dataset[sample(nrows), -excmeasures]
-    folds <- lapply(1:k, function(fold) (round(nrows/k*(fold-1))+1):round(nrows/k*fold))
-
-    folds <- lapply(folds, function(fold) list(
-      train = mldr_from_dataframe(dataset[-fold,], labels, mld$name),
-      test = mldr_from_dataframe(dataset[fold,], labels, mld$name)))
+    folds <- lapply(1:k,
+                    function(fold) (round(nrows/k*(fold-1))+1):round(nrows/k*fold))
   } else {
-    # Implement stratified kfolds
+    dataset <- mld$dataset[ , -excmeasures]
+    dataset$weight <- apply(dataset[,labels], 1, function(row)
+      Reduce(`*`, mld$labels$freq[row == 1]))
+    dataset <- dataset[order(dataset$weight), -length(dataset)]
+    strats <- lapply(1:k, function(strat)
+      sample((round(nrows/k*(strat-1))+1):round(nrows/k*strat)))
+    folds <- lapply(1:k, function(fold)
+      unlist(sapply(strats, function(strat)
+        strat[(round(length(strats[strat])/k*(fold-1))+1):round(length(strats[strat])/k*fold)])))
   }
+
+  folds <- lapply(folds, function(fold) list(
+    train = mldr_from_dataframe(dataset[-fold,], labels, mld$name),
+    test = mldr_from_dataframe(dataset[fold,], labels, mld$name)))
 
   class(folds) <- "mldr.folds"
   folds
 }
+
+
+
