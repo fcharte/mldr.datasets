33096c1ce314cfedde8d02bf6180eff03919af8c
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-20 09:59:07 +0100

    Refactors kfolds methods

--- R/kfolds.R
+++ R/kfolds.R
@@ -16,6 +16,31 @@
 #' }
 #' @export
 random.kfolds <- function(mld, k = 5, seed = 10) {
+  internal.kfolds(mld, k, seed, "random")
+}
+
+#' Partitions an mldr object into k folds
+#' @description This method partitions the given dataset into k folds using a stratified strategy, provinding training and test partitions for each fold.
+#' @param mld The \code{mldr} object to be partitioned
+#' @param k The number of folds to be generated. By default is 5
+#' @param seed The seed to initialize the random number generator. By default is 10. Change it if you want to obtain partitions containing
+#' different samples, for instance to use a 2x5 fcv strategy
+#' @return An \code{mldr.folds} object. This is a list containing k elements, one for each fold. Each element is made up
+#' of two mldr objects, called \code{train} and \code{test}
+#'  @examples
+#'\dontrun{
+#' library(mldr.datasets)
+#' library(mldr)
+#' folds.emotions <- stratified.kfolds(emotions)
+#' summary(folds.emotions[[1]]$train)
+#' summary(folds.emotions[[1]]$test)
+#' }
+#' @export
+stratified.kfolds <- function(mld, k = 5, seed = 10) {
+  internal.kfolds(mld, k, seed, "stratified")
+}
+
+internal.kfolds <- function(mld, k, seed, type = "random") {
   if (class(mld) != 'mldr')
     stop(paste(substitute(mld), "isn't an mldr object"))
 
@@ -26,14 +51,18 @@ random.kfolds <- function(mld, k = 5, seed = 10) {
   excmeasures <- (mld$measures$num.attributes+1):length(mld$dataset)
   nrows <- mld$measures$num.instances
   labels <- mld$labels$index
-  dataset <- mld$dataset[sample(nrows), -excmeasures]
-  folds <- lapply(1:k, function(fold) (round(nrows/k*(fold-1))+1):round(nrows/k*fold))
 
-  folds <- lapply(folds, function(fold) list(
-    train = mldr_from_dataframe(dataset[-fold,], labels, mld$name),
-    test = mldr_from_dataframe(dataset[fold,], labels, mld$name)))
+  if(type == "random") {
+    dataset <- mld$dataset[sample(nrows), -excmeasures]
+    folds <- lapply(1:k, function(fold) (round(nrows/k*(fold-1))+1):round(nrows/k*fold))
+
+    folds <- lapply(folds, function(fold) list(
+      train = mldr_from_dataframe(dataset[-fold,], labels, mld$name),
+      test = mldr_from_dataframe(dataset[fold,], labels, mld$name)))
+  } else {
+    # Implement stratified kfolds
+  }
 
   class(folds) <- "mldr.folds"
   folds
 }
-
