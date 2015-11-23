084aae6b8ca274b961c109208fb67a6143f39743
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:54:14 +0100

    Updates ohsumed documentation

--- R/ohsumed.R
+++ R/ohsumed.R
@@ -4,8 +4,10 @@
 #' @format An mldr object with 13929 instances, 1002 attributes and 23 labels
 #' @source Joachims, Thorsten, "Text Categorization with Suport Vector Machines: Learning with Many Relevant Features", in Proc. 10th European Conference on Machine Learning, pp. 137-142, 1998
 #' @examples
+#'\dontrun{
 #' ohsumed()  # Check and load the dataset
 #' toBibtex(ohsumed)
 #' ohsumed$measures
+#' }
 #' @export
 ohsumed <- function() check_n_load.mldr('ohsumed')
