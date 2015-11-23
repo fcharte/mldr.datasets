3dcb9cbb4b7938a4f2746cbd64a502c62532af5d
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:52:00 +0100

    Updates langlog documentation

--- R/langlog.R
+++ R/langlog.R
@@ -4,6 +4,8 @@
 #' @format An mldr object with 1460 instances, 1004 attributes and 75 labels
 #' @source Read, Jesse, "Scalable multi-label classification", University of Waikato, 2010
 #' @examples
+#'\dontrun{
 #' toBibtex(langlog)
 #' langlog$measures
+#' }
 "langlog"
