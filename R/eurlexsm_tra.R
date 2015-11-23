51f0028b3050d7b8368d7907e82a0fbf376483ef
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:48:30 +0100

    Updates eurlexsm_tra documentation

--- R/eurlexsm_tra.R
+++ R/eurlexsm_tra.R
@@ -4,8 +4,10 @@
 #' @format An mldr object with 17413 instances, 5000 attributes and 201 labels
 #' @source Mencia, E. L. and Furnkranz, J., "Efficient pairwise multilabel classification for large-scale problems in the legal domain", Machine Learning and Knowledge Discovery in Databases, pp. 50--65, 2008
 #' @examples
+#'\dontrun{
 #' eurlexsm_tra()  # Check and load the dataset
 #' toBibtex(eurlexsm_tra[[1]])
 #' eurlexsm_tra[[1]]$measures
+#' }
 #' @export
 eurlexsm_tra <- function() check_n_load.mldr('eurlexsm_tra')
