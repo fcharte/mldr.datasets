6e822090679fccd128fcfef4652473d498a85aa8
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 10:07:36 +0100

    Updates yahoo_society documentation

--- R/yahoo_society.R
+++ R/yahoo_society.R
@@ -4,8 +4,10 @@
 #' @format An mldr object with 14512 instances, 31802 attributes and 27 labels
 #' @source Ueda, N. and Saito, K., "Parametric mixture models for multi-labeled text", Advances in neural information processing systems, pp. 721--728, 2002
 #' @examples
+#'\dontrun{
 #' yahoo_society()  # Check and load the dataset
 #' toBibtex(yahoo_society)
 #' yahoo_society$measures
+#' }
 #' @export
 yahoo_society <- function() check_n_load.mldr('yahoo_society')
