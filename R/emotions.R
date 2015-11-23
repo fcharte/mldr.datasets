8f977a666216ff9bab43c6e8f6a5b415e4dc63b9
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:44:48 +0100

    Updates emotions documentation

--- R/emotions.R
+++ R/emotions.R
@@ -4,6 +4,8 @@
 #' @format An mldr object with 593 instances, 72 attributes and 6 labels
 #' @source Wieczorkowska, A. and Synak, P. and Ra'{s}, Z., "Multi-Label Classification of Emotions in Music", Intelligent Information Processing and Web Mining, Vol. 35, Chap. 30, pp. 307-315, 2006
 #' @examples
+#'\dontrun{
 #' toBibtex(emotions)
 #' emotions$measures
+#' }
 "emotions"
