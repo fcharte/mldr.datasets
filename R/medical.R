f6cf273311a8b86b061b5ab55278ce1ff9ae1e02
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:52:41 +0100

    Updates medical documentation

--- R/medical.R
+++ R/medical.R
@@ -4,6 +4,8 @@
 #' @format An mldr object with 978 instances, 1449 attributes and 45 labels
 #' @source Crammer, K. and Dredze, M. and Ganchev, K. and Talukdar, P. P. and Carroll, S., "Automatic Code Assignment to Medical Text", in Proc. Workshop on Biological, Translational, and Clinical Language Processing,  Prague, Czech Republic, BioNLP07, pp. 129-136, 2007
 #' @examples
+#'\dontrun{
 #' toBibtex(medical)
 #' medical$measures
+#' }
 "medical"
