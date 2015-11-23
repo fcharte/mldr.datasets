d80a6a480a849774c8ff548f564fa5801c2e59c0
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 10:32:53 +0100

    Fix param name in toBibtex documentation

--- R/toBibTeX.R
+++ R/toBibTeX.R
@@ -1,6 +1,6 @@
 #' Returns the BibTeX entry associated to the mldr object given as input
 #' @description Gets the content of the \code{bibtex} member of the \code{mldr} object and returns it
-#' @param mldr The mldr object whose BibTeX entry is needed
+#' @param mld The mldr object whose BibTeX entry is needed
 #' @return A string with the BibTeX entry
 #' @examples
 #' library(mldr.datasets)
