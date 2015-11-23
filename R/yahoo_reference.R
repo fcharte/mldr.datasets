3111c8f6b516d40bcd31adea07eb60fc62becbca
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 10:06:33 +0100

    Updates yahoo_reference documentation

--- R/yahoo_reference.R
+++ R/yahoo_reference.R
@@ -4,8 +4,10 @@
 #' @format An mldr object with 8027 instances, 39679 attributes and 33 labels
 #' @source Ueda, N. and Saito, K., "Parametric mixture models for multi-labeled text", Advances in neural information processing systems, pp. 721--728, 2002
 #' @examples
+#'\dontrun{
 #' yahoo_reference()  # Check and load the dataset
 #' toBibtex(yahoo_reference)
 #' yahoo_reference$measures
+#' }
 #' @export
 yahoo_reference <- function() check_n_load.mldr('yahoo_reference')
