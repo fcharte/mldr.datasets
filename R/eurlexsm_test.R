29a31748070e0f1dc913630345504ce975a2819d
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:48:06 +0100

    Updates eurlexsm_test documentation

--- R/eurlexsm_test.R
+++ R/eurlexsm_test.R
@@ -4,8 +4,10 @@
 #' @format An mldr object with 1935 instances, 5000 attributes and 201 labels
 #' @source Mencia, E. L. and Furnkranz, J., "Efficient pairwise multilabel classification for large-scale problems in the legal domain", Machine Learning and Knowledge Discovery in Databases, pp. 50--65, 2008
 #' @examples
+#'\dontrun{
 #' eurlexsm_test()  # Check and load the dataset
 #' toBibtex(eurlexsm_test[[1]])
 #' eurlexsm_test[[1]]$measures
+#' }
 #' @export
 eurlexsm_test <- function() check_n_load.mldr('eurlexsm_test')
