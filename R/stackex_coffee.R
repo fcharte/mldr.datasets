4b391f6de0f5e58712dc651122e379133464383f
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 11:04:13 +0100

    Moves stackex_coffee to additional datasets folder

--- R/stackex_coffee.R
+++ R/stackex_coffee.R
@@ -5,7 +5,9 @@
 #' @source Charte, Francisco and Rivera, Antonio J. and del Jesus, Maria J. and Herrera, Francisco, "QUINTA: A question tagging assistant to improve the answering ratio in electronic forums", in EUROCON 2015 - International Conference on Computer as a Tool (EUROCON), IEEE, pp. 1-6, 2015
 #' @examples
 #'\dontrun{
+#' stackex_coffee()
 #' toBibtex(stackex_coffee)
 #' stackex_coffee$measures
 #' }
-"stackex_coffee"
+#' @export
+stackex_coffee <- function() check_n_load.mldr('stackex_coffee')
