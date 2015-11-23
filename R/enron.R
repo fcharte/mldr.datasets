7237e44ea1d445ab656887a1bfe1d37ec3161786
Francisco Charte Ojeda <francisco@fcharte.com>
2015-11-23 09:45:36 +0100

    Updates enron documentation

--- R/enron.R
+++ R/enron.R
@@ -4,7 +4,10 @@
 #' @format An mldr object with 1702 instances, 1001 attributes and 53 labels
 #' @source Klimt, B. and Yang, Y., "The Enron Corpus: A New Dataset for Email Classification Research", in Proc. ECML04, Pisa, Italy, pp. 217-226, 2004
 #' @examples
+#'\dontrun{
 #' enron()  # Check and load the dataset
 #' toBibtex(enron)
 #' enron$measures
+#' }
+#' @export
 enron <- function() check_n_load.mldr("enron")
