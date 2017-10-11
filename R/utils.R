9ea3a8fd1ec13dad6e7e4ac73bd946ed0329b7fc
David Charte <fdavidcl@protonmail.com>
2017-10-11 13:49:26 +0200

    Add functions to download rds datasets
    Deprecates previous functions to download in rda format

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,75 @@
-#' @title Check if an mldr object is locally available and download it if needed
+RDS_CSV <- "https://fcharte.github.io/mldr.datasets/datasets_rds.csv"
+
+#' @title Get a multilabel dataset by name
+#' @description \code{get.mldr} obtains a multilabel dataset, either by finding it in the download directory or downloading it.
+#' @param name Name of the dataset to load
+#' @examples
+#'\dontrun{
+#' library(mldr.datasets)
+#' bibtex <- get.mldr("bibtex")
+#' bibtex$measures
+#' }
+#' @export
+get.mldr <- function(name,
+                     download.dir = if (is.null(getOption("mldr.download.dir")))
+                       file.path(normalizePath("~"), ".mldr", "datasets")
+                     else
+                       getOption("mldr.download.dir")) {
+  if (exists(name, .GlobalEnv, mode = "list"))
+    return(get(name))
+
+  filename <- file.path(download.dir, paste0(name, ".rds"))
+
+  cat("Looking for dataset", name, "in the download directory\n")
+
+  if (dir.exists(download.dir)) {
+    if (file.exists(filename)) {
+      cat("Now reading dataset", name, "\n")
+      return(readRDS(filename))
+    }
+  } else {
+    cat("Download directory not found. Creating", download.dir, "\n")
+    dir.create(download.dir)
+  }
+
+  cat("Looking for dataset", name, "online...\n")
+  available <- read.csv(RDS_CSV, stringsAsFactors = FALSE)
+  url <- available[available$Name == name, "URL"]
+
+  if (length(url) > 0) {
+    cat("Downloading dataset", name, "\n")
+    retValue <- NULL
+
+    tryCatch(
+      retValue <- download.file(url, destfile = filename),
+      error = function(...) {
+        cat("Attempt to download dataset", name, "failed\n")
+      }
+    )
+
+    return(readRDS(filename))
+  } else {
+    cat("Download URL not found. Dataset",
+        name,
+        "may not be available at the repository\n")
+  }
+}
+
+#' @title Obtain additional datasets available to download
+#' @description \code{available.mldrs} retrieves the most up to date list of additional datasets. Those datasets are not
+#' included into the package, but can be downloaded and saved locally.
+#' @return A data.frame with the available multilabel datasets
+#' @examples
+#'\dontrun{
+#' library(mldr.datasets)
+#' names <- available.mldrs()$Name
+#' }
+#' @export
+available.mldrs <- function() {
+  read.csv(RDS_CSV, stringsAsFactors = FALSE)
+}
+
+#' @title (Deprecated) Check if an mldr object is locally available and download it if needed
 #' @description This function checks if the mldr object whose name is given as input is locally available, loading it in memory.
 #' If necessary, the dataset will be downloaded from the GitHub repository and saved locally.
 #' @param mldr.name Name of the dataset to load
@@ -10,6 +81,7 @@
 #' }
 #' @export
 check_n_load.mldr <- function(mldr.name) {
+  .Deprecated("get.mldr", "mldr.datasets", old = "check_n_load.mldr")
   if(exists(mldr.name, .GlobalEnv, mode = "list"))
     cat('The ', mldr.name, ' dataset is already loaded.')
   else {
@@ -30,7 +102,7 @@ check_n_load.mldr <- function(mldr.name) {
   }
 }
 
-#' @title Obtain and show a list of additional datasets available to download
+#' @title (Deprecated) Obtain and show a list of additional datasets available to download
 #' @description The function downloads from GitHub the most up to date list of additional datasets. Those datasets are not
 #' included into the package, but can be downloaded and saved locally.
 #' @examples
@@ -40,8 +112,10 @@ check_n_load.mldr <- function(mldr.name) {
 #' }
 #' @export
 mldrs <- function() {
+  .Deprecated("available.mldrs", "mldr.datasets")
   availableMlds <- read.csv("https://fcharte.github.io/mldr.datasets/availableMlds.csv", stringsAsFactors = FALSE)
   save(availableMlds, file = paste0(find.package('mldr.datasets'), "/R/sysdata.rda"), compress = "gzip")
 
   View(availableMlds[-(length(availableMlds))], 'List of additional datasets available at the mldr.datasets repository')
 }
+
